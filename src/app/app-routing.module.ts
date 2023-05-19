bb2c3036e082fd15905f82836308f49d78e9760e
aravind <aravindm@dynapt.ai>
2023-05-20 01:43:54 +0530

    Aravind_blog_commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CampusComponent } from './campus/campus.component';
 import { HomeComponent } from './home/home.component';
+import { AdmissionComponent } from './admission/admission.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { BlogComponent } from './blog/blog.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'campus', component: CampusComponent },
+  { path: 'admission', component: AdmissionComponent },
+  { path: 'curriculum', component: CurriculumComponent },
+  { path: 'blog', component: BlogComponent },
 
 ];
 
