17eab1f9471d4c1f75d632ec4918c7dff7a92c14
aravind <aravindm@dynapt.ai>
2023-06-22 01:04:51 +0530

    css_bootstrap

--- src/app/campus/campus.component.ts
+++ src/app/campus/campus.component.ts
@@ -15,8 +15,8 @@ export class CampusComponent {
       image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
       thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg'
     }, {
-      image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-      thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
+      image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
+      thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/1.jpg',
       title: 'Example with title.'
     }, {
       image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/7.jpg',
@@ -30,11 +30,11 @@ export class CampusComponent {
       thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/2.jpg',
       title: 'Example two with title.'
     },{
-      image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
-      thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg'
+      image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
+      thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg'
     }, {
-      image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
-      thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/4.jpg',
+      image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
+      thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/9.jpg',
       title: 'Example with title.'
     },];
 }
